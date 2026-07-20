a2ecbb8d573d633900e90df7bf5271ae511ae56c
Son Vichet <vichetson.id88@gmail.com>
2026-07-20 16:21:03 +0700

    Vichet fisrt push

---
+++ Vichet_Son.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    system("cls");
+
+    cout << endl;
+    cout << "Hello teacher , My name is Vichet Son !! " << endl;
+    cout << endl;
+}
