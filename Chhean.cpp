bc6368175e6f29784550621d969e9f2405c97e20
chheandy5-sys <chheandy5@gmail.com>
2026-07-20 16:50:18 +0700

    second

---
+++ Chhean.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+
+    system("cls");
+
+    cout << endl;
+    cout << "Hello teacher , My name is Dy chhean !! " << endl;
+    cout << endl;
+}
